c616a5c25eacf51b82471582a94d3dd09a675e5b
arpruss <arpruss@gmail.com>
2019-09-08 09:57:34 -0500

    resizeicons

--- makeicons.sh
+++ makeicons.sh
@@ -1,6 +1,10 @@
 for x in settings menu; do
-    convert $x.png -resize 96x96 app/src/main/res/drawable-xxhdpi/$x.png
-    convert $x.png -resize 64x64 app/src/main/res/drawable-xhdpi/$x.png
-    convert $x.png -resize 48x48 app/src/main/res/drawable-hdpi/$x.png
-    convert $x.png -resize 32x32 app/src/main/res/drawable-mdpi/$x.png
+#    convert $x.png -resize 96x96 app/src/main/res/drawable-xxhdpi/$x.png
+#    convert $x.png -resize 64x64 app/src/main/res/drawable-xhdpi/$x.png
+#    convert $x.png -resize 48x48 app/src/main/res/drawable-hdpi/$x.png
+#    convert $x.png -resize 32x32 app/src/main/res/drawable-mdpi/$x.png
+    convert $x.png -resize 120x120 app/src/main/res/drawable-xxhdpi/$x.png
+    convert $x.png -resize 80x80 app/src/main/res/drawable-xhdpi/$x.png
+    convert $x.png -resize 60x60 app/src/main/res/drawable-hdpi/$x.png
+    convert $x.png -resize 40x40 app/src/main/res/drawable-mdpi/$x.png
 done
